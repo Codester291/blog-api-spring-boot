aa9fba677b5971f4be635e4912aecd1f5ae7a11d
Codester291 <Codester291@gmail.com>
2020-05-23 01:14:05 +0100

    Added @Autowired

--- src/main/java/com/example/blogapi/controller/BlogController.java
+++ src/main/java/com/example/blogapi/controller/BlogController.java
@@ -3,6 +3,7 @@ package com.example.blogapi.controller;
 import com.example.blogapi.model.Blog;
 import com.example.blogapi.model.CustomResponse;
 import com.example.blogapi.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import java.util.Optional;
 @RequestMapping("/blog")
 public class BlogController {
 
+    @Autowired
     private final BlogService blogService;
 
     public BlogController(BlogService blogService) {
